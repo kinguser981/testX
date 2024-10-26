740d7bea57fa842ff84d9d23c136dcfa4bc608bc
Samuel Kendall <kinguser981@gmail.com>
2024-10-26 19:53:50 +0330

    Update convertor.py

--- convertor.py
+++ convertor.py
@@ -1,3 +1,4 @@
+import sys
 import re
 
 def extract_file_id(url):
@@ -15,12 +16,15 @@ def extract_file_id(url):
   return match[0] if match else None
 
 if __name__ == "__main__":
-  url = input("Enter the Google Drive URL: ")
+  # Check if at least one argument is provided (excluding the script name)
+  if len(sys.argv) < 2:
+    print("Usage: python file.py <URL>")
+    sys.exit(1)
+
+  url = sys.argv[1]
   file_id = extract_file_id(url)
 
   if file_id:
-
     print(f'"FILE_ID={file_id}" >> $GITHUB_ENV')
-    
   else:
     print("Invalid Google Drive URL")
